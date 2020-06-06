b8c2ed6a7e828f84920469c0c60f6fe93722e1ea
Clive Gout <35511400+cliveontoast@users.noreply.github.com>
2020-06-06 14:17:17 +0800

    Displayed date on UI is sent to server i.e. when displaying 2020 June 06, it will send 2020-06-06 in the http request
    Signed-off-by: Clive Gout <35511400+cliveontoast@users.noreply.github.com>

--- Cloud.WebApp/ClientApp/src/app/services/voltageSummary.service.ts
+++ Cloud.WebApp/ClientApp/src/app/services/voltageSummary.service.ts
@@ -18,8 +18,6 @@ export class VoltageSummaryService {
   }
 
   private getString(date: Date): string {
-    const offset = date.getTimezoneOffset()
-    date = new Date(date.getTime() + (offset*60*1000))
     return date.toISOString().split('T')[0]
   }
 }
