c9913cfa7fef6e588a6d6002a325e1cdbbbbddf1
cliveontoast <35511400+cliveontoast@users.noreply.github.com>
2021-05-14 23:11:46 +0800

    add files

---
+++ TeslaPowerwallSource/Client.cs
@@ -0,0 +1,122 @@
+﻿using LazyCache;
+using Newtonsoft.Json;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TeslaPowerwallSource
+{
+    public class Client : IClient
+    {
+        public const string LOGGING = "TeslaClient: ";
+        private static readonly object __lock = new object();
+        private readonly IAppCache _appCache;
+        private readonly ILogger _logger;
+        private readonly ITeslaPowerwallSettings _settings;
+        private readonly IEndPoint _endPoint;
+
+        public Client(IAppCache appCache, ITeslaPowerwallSettings settings, ILogger logger, IEndPoint endPoint)
+        {
+            _appCache = appCache;
+            _settings = settings;
+            _logger = logger;
+            _endPoint = endPoint;
+        }
+
+        public HttpClient Build(CancellationToken token)
+        {
+            lock (__lock)
+            {
+                var clientTask = _appCache.GetOrAddAsync("telsaClient", async () => await GetHttpClient(token));
+                var client = RunWait(clientTask);
+                return client;
+            }
+        }
+
+        public static TResult RunWait<TResult>(Task<TResult> asyncTask)
+        {
+            var task = Task.Run(() => asyncTask);
+            task.Wait();
+            return task.Result;
+        }
+
+        private async Task<HttpClient> GetHttpClient(CancellationToken token)
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Add("Accept", "*/*");
+            client.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate, br");
+            client.DefaultRequestHeaders.Add("Accept-Language", "en-GB,en;q=0.5");
+            client.DefaultRequestHeaders.Add("Cache-Control", "max-age=0");
+            client.DefaultRequestHeaders.Add("Connection", "keep-alive");
+            client.DefaultRequestHeaders.Add("DNT", "1");
+            client.DefaultRequestHeaders.Add("Host", _settings.IP);
+            client.DefaultRequestHeaders.Add("Referer", $"https://{_settings.IP}/");
+            client.DefaultRequestHeaders.Add("Sec-GPC", "1");
+            client.DefaultRequestHeaders.Add("TE", "Trailers");
+            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0");
+            client.DefaultRequestHeaders.Add("Cookie", await GetAuthCookies(client, token));
+            return client;
+        }
+
+        private async Task<string> GetAuthCookies(HttpClient client, CancellationToken token)
+        {
+            var authToken = await GetAuthHeaders(client, token);
+            var result = string.Join("; ", authToken);
+            _logger.Debug(LOGGING + "auth cookie text {CookieContent}", result);
+            return result;
+        }
+
+        private async Task<string[]> GetAuthHeaders(HttpClient client, CancellationToken token)
+        {
+            _logger.Information(LOGGING + "Fetching auth cookies");
+            if (string.IsNullOrWhiteSpace(_settings.IP))
+            {
+                var txt = LOGGING + $"Setting is unspecified '{_settings.IP}'";
+                _logger.Fatal(txt);
+                throw new Exception(txt);
+            }
+            if (string.IsNullOrWhiteSpace(_settings.Password))
+            {
+                var txt = LOGGING + $"Setting is unspecified '{_settings.Password}'";
+                _logger.Fatal(txt);
+                throw new Exception(txt);
+            }
+            try
+            {
+                var requestContent = JsonConvert.SerializeObject(new BasicAuthRequestContent { email = _settings.Email, password = _settings.Password });
+                var httpContent = new StringContent(requestContent, Encoding.UTF8, "application/json");
+                var endPoint = _endPoint.GetEndPoint<BasicAuthResponse>();
+                string requestUri = $"https://{_settings.IP}{endPoint}";
+                _logger.Information(LOGGING + "Auth sending to {Http} Content: {Content}", requestUri, requestContent);
+                var response = await client.PostAsync(requestUri, httpContent, token);
+                if (!IsOk(response))
+                    throw new InvalidOperationException($"Not ok {response.StatusCode}");
+                if (!response.Headers.TryGetValues("Set-Cookie", out var authCookies))
+                    throw new KeyNotFoundException("Set-Cookie");
+                _logger.Information(LOGGING + "Auth response ok {@Cookie}", authCookies);
+                return authCookies.Select(a => a.Replace("; Path=/", "")).ToArray();
+            }
+            catch (Exception e)
+            {
+                _logger.Error(e, LOGGING + "Auth exception");
+                throw;
+            }
+        }
+
+        private bool IsOk(HttpResponseMessage response)
+        {
+            return _endPoint.IsOk(response);
+        }
+    }
+
+    public interface IClient
+    {
+        HttpClient Build(CancellationToken token);
+    }
+}
---
+++ TeslaPowerwallSource/EndPoint.cs
@@ -0,0 +1,44 @@
+﻿using Serilog;
+using System;
+using System.Net.Http;
+using TeslaEntities;
+
+namespace TeslaPowerwallSource
+{
+    public class EndPoint : IEndPoint
+    {
+        private readonly ILogger _logger;
+
+        public EndPoint(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public string GetEndPoint<TResponse>() where TResponse : WebResponse
+        {
+            var type = typeof(TResponse);
+            if (type.Equals(typeof(MetersAggregates)))
+                return "/api/meters/aggregates";
+            else if (type.Equals(typeof(StateOfEnergy)))
+                return "/api/system_status/soe";
+            else if (type.Equals(typeof(BasicAuthResponse)))
+                return "/api/login/Basic";
+            throw new NotImplementedException(type.FullName);
+        }
+
+        public bool IsOk(HttpResponseMessage response)
+        {
+            if (response.Content != null && response.IsSuccessStatusCode)
+                return true;
+
+            _logger.Warning("tesla powerwall response was not ok {StatusCode} {@Response}", response.StatusCode, response);
+            return false;
+        }
+    }
+
+    public interface IEndPoint
+    {
+        string GetEndPoint<TResponse>() where TResponse : WebResponse;
+        bool IsOk(HttpResponseMessage response);
+    }
+}
